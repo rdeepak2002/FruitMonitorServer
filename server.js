c5f43541444ed11cdc4193648649f9bdc615f797
rdeepak2002 <rdeepak2002@gmail.com>
2021-05-22 23:54:55 -0700

    adeed scokets

--- server.js
+++ server.js
@@ -5,41 +5,49 @@ const eventHubsCompatiblePath = "iothub-ehub-fruithub-11240618-c3fa7f04ea";
 const iotHubSasKey = "6YX/DiJYZRJrcacBH2sOiDdL5hjgS0d9RJv42/Z6pqo=";
 const connectionString = `Endpoint=${eventHubsCompatibleEndpoint};EntityPath=${eventHubsCompatiblePath};SharedAccessKeyName=service;SharedAccessKey=${iotHubSasKey}`;
 
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 var printError = function (err) {
     console.log(err.message);
-  };
+};
 
-  var printMessages = function (messages) {
+var printMessages = function (messages) {
     for (const message of messages) {
-    //   console.log("Telemetry received: ");
-      console.log(message.body.imageUrl);
-
-      console.log();
-
-    //   const data = JSON.parse(message.body);
-    //   console.log(data);
-    //   console.log("Properties (set by device): ");
-    //   console.log(JSON.stringify(message.properties));
-    //   console.log("System properties (set by IoT Hub): ");
-    //   console.log(JSON.stringify(message.systemProperties));
-    //   console.log("");
-      console.log();
+        console.log(message.body.imageUrl);
+        console.log();
     }
-  };
-  
-  async function main() {
+};
+
+async function main() {
     console.log("IoT Hub Quickstarts - Read device to cloud messages.");
-  
+
     const clientOptions = {};
     const consumerClient = new EventHubConsumerClient("$Default", connectionString, clientOptions);
-  
+
     consumerClient.subscribe({
-      processEvents: printMessages,
-      processError: printError,
+        processEvents: printMessages,
+        processError: printError
     });
-  }
-  
-  main().catch((error) => {
-    console.error("Error running sample:", error);
-  });
-  
+}
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+
+    main().catch((error) => {
+        console.error("Error running sample:", error);
+    });
+});
